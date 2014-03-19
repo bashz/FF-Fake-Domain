737129659f791243b82efdb1f867ba6b9b0418c0
Bechir <bechir@accessnow.org>
2014-03-19 04:09:51 +0100

    Fixes #6

--- data/js/setting.js
+++ data/js/setting.js
@@ -1,47 +1,48 @@
 var table = document.getElementById("table");
-self.port.on("buildForm", function(discovered) {
+
+self.port.on("buildForm", function(discovered, allow, forbidden, norule) {
     for (var i = 0; i < discovered.length; i++) {
         var tr = document.createElement("tr");
         var tdDomain = document.createElement("td");
         var tdAllowed = document.createElement("td");
-        var tdForbiden = document.createElement("td");
+        var tdforbidden = document.createElement("td");
         var tdNoRule = document.createElement("td");
         var allowedRadio = document.createElement("input");
-        var forbidenRadio = document.createElement("input");
+        var forbiddenRadio = document.createElement("input");
         var noRuleRadio = document.createElement("input");
         tr.class = "tr" + (i % 2);
         tdDomain.class = "domain";
         allowedRadio.type = "radio";
-        forbidenRadio.type = "radio";
+        forbiddenRadio.type = "radio";
         noRuleRadio.type = "radio";
         allowedRadio.id = "allowed" + "-" + discovered[i].name;
-        forbidenRadio.id = "forbiden" + "-" + discovered[i].name;
+        forbiddenRadio.id = "forbidden" + "-" + discovered[i].name;
         noRuleRadio.id = "norule" + "-" + discovered[i].name;
         if(discovered[i].rule === "allowed"){
             allowedRadio.checked = "true";
         }
-        else if(discovered[i].rule === "forbiden"){
-            forbidenRadio.checked = "true";
+        else if(discovered[i].rule === "forbidden"){
+            forbiddenRadio.checked = "true";
         }
         else{
             noRuleRadio.checked = "true";
         }
         allowedRadio.value = "allowed";
-        forbidenRadio.value = "forbiden";
+        forbiddenRadio.value = "forbidden";
         noRuleRadio.value = "norule";
         allowedRadio.name = discovered[i].name;
-        forbidenRadio.name = discovered[i].name;
+        forbiddenRadio.name = discovered[i].name;
         noRuleRadio.name = discovered[i].name;
         tdDomain.appendChild(document.createTextNode(discovered[i].name));
         tdAllowed.appendChild(allowedRadio);
-        tdAllowed.appendChild(document.createTextNode("Allowed"));
-        tdForbiden.appendChild(forbidenRadio);
-        tdForbiden.appendChild(document.createTextNode("Forbiden"));
+        tdAllowed.appendChild(document.createTextNode(allow));
+        tdforbidden.appendChild(forbiddenRadio);
+        tdforbidden.appendChild(document.createTextNode(forbidden));
         tdNoRule.appendChild(noRuleRadio);
-        tdNoRule.appendChild(document.createTextNode("No Rule"));
+        tdNoRule.appendChild(document.createTextNode(norule));
         tr.appendChild(tdDomain);
         tr.appendChild(tdAllowed);
-        tr.appendChild(tdForbiden);
+        tr.appendChild(tdforbidden);
         tr.appendChild(tdNoRule);
         table.appendChild(tr);
     }
